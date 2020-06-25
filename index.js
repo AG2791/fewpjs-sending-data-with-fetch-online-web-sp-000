544e48d22cb62940f83fc6ee1fc223b1e6ec8a5c
AJ Guiles <ashiguiles@gmail.com>
2020-06-25 18:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+// Add your code here
+function submitData( name, email ) {
+               
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+         //data sent  between client and server needs to be a string, hence stringify.
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+    //   this is the reponse from server =, it converts the body of the response from JSON to a plain old JavaScript object
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+        //   diplay server response 
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+//    error in fetch(), using catch prevents JS from failing silently. 
+
+  }
